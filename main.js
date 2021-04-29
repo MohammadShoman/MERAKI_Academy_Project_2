7eb8069c7a457427b1f972543e7a7f3a6b820369
MohammadShoman <mohammad.shoman25@gmail.com>
2021-04-29 01:29:24 -0700

    added header

--- main.js
+++ main.js
@@ -1,21 +1,3 @@
 console.log("e-commerce");
 
-const allItems = [
-  { name: "Tshirt", description: "use it", img: "./images/download.jfif" },
-];
-const items = $("#items");
-const getItems = () => {
-  for (i = 0; i < allItems.length; i++) {
-    const myItem = $(`<div >
-    <img src=${allItems[i]["img"]}>
-    <p>${allItems[i]["name"]}</p>
-</div>`);
-myItem.appendTo(items);
-  }
-  
-};
 
-
-window.onload=()=>{
-    getItems();
-}

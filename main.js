325879b9482b05ef7cea861ea83b6f6e386aa0f8
MohammadShoman <mohammad.shoman25@gmail.com>
2021-04-29 16:38:59 -0700

    added items

--- main.js
+++ main.js
@@ -6,9 +6,26 @@ const allItems = [
     { name: "Tshirt", description: "use it", img: "./images./img_2.JPG" },
     { name: "Tshirt", description: "use it", img: "./images/img_3.jfif" },
     { name: "Tshirt", description: "use it", img: "./images/img_4.jfif" },
+    { name: "Tshirt", description: "use it", img: "./images/img_4.jfif" },
+  ],
+  [
+    { name: "Tshirt", description: "use it", img: "./images/download.jfif" },
+    { name: "Tshirt", description: "use it", img: "./images./img_2.JPG" },
+    { name: "Tshirt", description: "use it", img: "./images/img_3.jfif" },
+    { name: "Tshirt", description: "use it", img: "./images/img_4.jfif" },
+    { name: "Tshirt", description: "use it", img: "./images/img_4.jfif" },
+  ],
+  [
+    { name: "Tshirt", description: "use it", img: "./images/download.jfif" },
+    { name: "Tshirt", description: "use it", img: "./images./img_2.JPG" },
+    { name: "Tshirt", description: "use it", img: "./images/img_3.jfif" },
+    { name: "Tshirt", description: "use it", img: "./images/img_4.jfif" },
+    { name: "Tshirt", description: "use it", img: "./images/img_4.jfif" },
   ],
 ];
 const items = $("#items");
+const items2 = $("#items2");
+const items3 = $("#items3");
 const getItems = () => {
   for (i = 0; i < allItems[0].length; i++) {
     const myItem = $(`<div class="item">
@@ -18,6 +35,25 @@ const getItems = () => {
     <button>cart</button>
 </div>`);
     myItem.appendTo(items);
+  }
+
+  for (i = 0; i < allItems[1].length; i++) {
+    const myItem = $(`<div class="item">
+    <img class="image" src=${allItems[1][i]["img"]}>
+    <p>${allItems[1][i]["name"]}</p>
+    <p>${allItems[1][i]["description"]}</p>
+    <button>cart</button>
+</div>`);
+    myItem.appendTo(items2);
+  }
+  for (i = 0; i < allItems[1].length; i++) {
+    const myItem = $(`<div class="item">
+    <img class="image" src=${allItems[2][i]["img"]}>
+    <p>${allItems[2][i]["name"]}</p>
+    <p>${allItems[2][i]["description"]}</p>
+    <button>cart</button>
+</div>`);
+    myItem.appendTo(items3);
   }
 };
 

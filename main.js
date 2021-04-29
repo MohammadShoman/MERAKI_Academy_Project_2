4e9d62a59ce37f1ec7bb74970f22d5fa3c4047a7
MohammadShoman <mohammad.shoman25@gmail.com>
2021-04-29 01:32:51 -0700

    add first itme

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
 console.log("e-commerce");
 
 
+const allItems = [
+  { name: "Tshirt", description: "use it", img: "./images/download.jfif", },
+];
+const items = $("#items");
+const getItems = () => {
+  for (i = 0; i < allItems.length; i++) {
+    const myItem = $(`<div class="item">
+    <img class="image" src=${allItems[i]["img"]}>
+    <p>${allItems[i]["name"]}</p>
+    <p>${allItems[i]["description"]}</p>
+    <button>cart</button>
+</div>`);
+myItem.appendTo(items);
+  }
+  
+};
+
+
+
+  getItems()
+

e529cb469a594b079b8b917efd67fed9f5277d1b
MohammadShoman <mohammad.shoman25@gmail.com>
2021-04-29 15:23:30 -0700

    added items

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 console.log("e-commerce");
 
-
 const allItems = [
-  { name: "Tshirt", description: "use it", img: "./images/download.jfif", },
+  [
+    { name: "Tshirt", description: "use it", img: "./images/download.jfif" },
+    { name: "Tshirt", description: "use it", img: "./images./img_2.JPG" },
+    { name: "Tshirt", description: "use it", img: "./images/img_3.jfif" },
+    { name: "Tshirt", description: "use it", img: "./images/img_4.jfif" },
+  ],
 ];
 const items = $("#items");
 const getItems = () => {
-  for (i = 0; i < allItems.length; i++) {
+  for (i = 0; i < allItems[0].length; i++) {
     const myItem = $(`<div class="item">
-    <img class="image" src=${allItems[i]["img"]}>
-    <p>${allItems[i]["name"]}</p>
-    <p>${allItems[i]["description"]}</p>
+    <img class="image" src=${allItems[0][i]["img"]}>
+    <p>${allItems[0][i]["name"]}</p>
+    <p>${allItems[0][i]["description"]}</p>
     <button>cart</button>
 </div>`);
-myItem.appendTo(items);
+    myItem.appendTo(items);
   }
-  
 };
 
-
-
-  getItems()
-
+getItems();

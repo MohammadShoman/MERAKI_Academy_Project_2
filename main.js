d4617e8cb7d17558a5ba316bc6db86cf02ab3fb3
MohammadShoman <mohammad.shoman25@gmail.com>
2021-04-30 01:13:55 -0700

    added abackground image show

--- main.js
+++ main.js
@@ -7,25 +7,50 @@ const allItems = [
     { name: "Tshirt", description: "use it", img: "./images/img_3.jfif" },
     { name: "Tshirt", description: "use it", img: "./images/img_4.jfif" },
     { name: "Tshirt", description: "use it", img: "./images/img_5.jfif" },
+    { name: "Tshirt", description: "use it", img: "./images/img_6.jfif" }
   ],
   [
-    { name: "Tshirt", description: "use it", img: "./images/jeans1.jfif" },
-    { name: "Tshirt", description: "use it", img: "./images./jeans2.jfif" },
-    { name: "Tshirt", description: "use it", img: "./images/jeans3.jfif" },
-    { name: "Tshirt", description: "use it", img: "./images/jeans4.jfif" },
-    { name: "Tshirt", description: "use it", img: "./images/jeans5.jfif" },
+    { name: "Jeans", description: "use it", img: "./images/jeans1.jfif" },
+    { name: "Jeans", description: "use it", img: "./images./jeans2.jfif" },
+    { name: "Jeans", description: "use it", img: "./images/jeans3.jfif" },
+    { name: "Jeans", description: "use it", img: "./images/jeans4.jfif" },
+    { name: "Jeans", description: "use it", img: "./images/jeans5.jfif" },
+    { name: "Jeans", description: "use it", img: "./images/jeans6.jfif" }
   ],
   [
-    { name: "Tshirt", description: "use it", img: "./images/shoes.jfif" },
-    { name: "Tshirt", description: "use it", img: "./images./shoes1.JPG" },
-    { name: "Tshirt", description: "use it", img: "./images/shoes3.jfif" },
-    { name: "Tshirt", description: "use it", img: "./images/shoes4.jfif" },
-    { name: "Tshirt", description: "use it", img: "./images/shoes5.jfif" },
+    { name: "Shoes", description: "use it", img: "./images/shoes.jfif" },
+    { name: "Shoes", description: "use it", img: "./images./shoes1.JPG" },
+    { name: "Shoes", description: "use it", img: "./images/shoes3.jfif" },
+    { name: "Shoes", description: "use it", img: "./images/shoes4.jfif" },
+    { name: "Shoes", description: "use it", img: "./images/shoes5.jfif" },
+    { name: "Shoes", description: "use it", img: "./images/shoes6.jfif" }
   ],
 ];
 const items = $("#items");
 const items2 = $("#items2");
 const items3 = $("#items3");
+const slider_btn=$(".slider-btn")
+
+$(".slider-btn").on("click",function(){
+  $("#items").html("");
+
+  for (i = 0; i < allItems[0].length; i++) {
+    const myItem = $(`<div class="item">
+      <img class="image" src=${allItems[0][i]["img"]}>
+      <p>${allItems[0][i]["name"]}</p>
+      <p>${allItems[0][i]["description"]}</p>
+      <button>cart</button>
+  </div>`);
+    myItem.appendTo(items);
+  }
+  $("#items").show();
+  $("#items2").hide();
+  $("#items3").hide();
+});
+
+
+
+
 
 $("#t-shirt").on("click", function () {
   $("#items").html("");

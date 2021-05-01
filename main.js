cca8d7d57f4b41d433134a74ac7879d47493ad81
MohammadShoman <mohammad.shoman25@gmail.com>
2021-04-30 17:12:35 -0700

    edit slider

--- main.js
+++ main.js
@@ -107,13 +107,3 @@ $("#shoes").on("click", function () {
   
 });
 
-$(document).ready(function() {
-  $('#adaptive').lightSlider({
-      adaptiveHeight:true,
-      item:1,
-      slideMargin:0,
-      loop:true
-  });
-});
-
-

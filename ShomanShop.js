ce103eddc79de59d3233e96203f97256a5e9d985
MohammadShoman <mohammad.shoman25@gmail.com>
2021-05-02 04:28:06 -0700

    added all items in home

--- ShomanShop.js
+++ ShomanShop.js
@@ -2,28 +2,113 @@ console.log("e-commerce");
 
 const allItems = [
   [
-    { name: "T-shirt", price: "25.00 $", img: "./images/download.jfif" ,index:'0'},
-    { name: "T-shirt1", price: "17.00 $", img: "./images./img_2.JPG" ,index:'1'},
-    { name: "T-shirt2", price: "10.00 $", img: "./images/img_3.jfif" ,index:'2'},
-    { name: "T-shirt3", price: "10.00 $", img: "./images/img_4.jfif" ,index:'3'},
-    { name: "T-shirt4", price: "17.00 $", img: "./images/img_5.jfif" ,index:'4'},
-    { name: "T-shirt5", price: "25.00 $", img: "./images/img_6.jfif" ,index:'5'},
+    {
+      name: "T-shirt",
+      price: "25.00 $",
+      img: "./images/download.jfif",
+      index: "0",
+    },
+    {
+      name: "T-shirt1",
+      price: "17.00 $",
+      img: "./images./img_2.JPG",
+      index: "1",
+    },
+    {
+      name: "T-shirt2",
+      price: "10.00 $",
+      img: "./images/img_3.jfif",
+      index: "2",
+    },
+    {
+      name: "T-shirt3",
+      price: "10.00 $",
+      img: "./images/img_4.jfif",
+      index: "3",
+    },
+    {
+      name: "T-shirt4",
+      price: "17.00 $",
+      img: "./images/img_5.jfif",
+      index: "4",
+    },
+    {
+      name: "T-shirt5",
+      price: "25.00 $",
+      img: "./images/img_6.jfif",
+      index: "5",
+    },
   ],
   [
-    { name: "Jeans", price: "25.00 $", img: "./images/jeans1.jfif" ,index:'0'},
-    { name: "Jeans", price: "17.00 $", img: "./images./jeans2.jfif" ,index:'1'},
-    { name: "Jeans", price: "10.00 $", img: "./images/jeans3.jfif" ,index:'2'},
-    { name: "Jeans", price: "17.00 $", img: "./images/jeans4.jfif" ,index:'3'},
-    { name: "Jeans", price: "25.00 $", img: "./images/jeans5.jfif" ,index:'4'},
-    { name: "Jeans", price: "10.00 $", img: "./images/jeans6.jfif" ,index:'5'},
+    {
+      name: "Jeans",
+      price: "25.00 $",
+      img: "./images/jeans1.jfif",
+      index: "0",
+    },
+    {
+      name: "Jeans",
+      price: "17.00 $",
+      img: "./images./jeans2.jfif",
+      index: "1",
+    },
+    {
+      name: "Jeans",
+      price: "10.00 $",
+      img: "./images/jeans3.jfif",
+      index: "2",
+    },
+    {
+      name: "Jeans",
+      price: "17.00 $",
+      img: "./images/jeans4.jfif",
+      index: "3",
+    },
+    {
+      name: "Jeans",
+      price: "25.00 $",
+      img: "./images/jeans5.jfif",
+      index: "4",
+    },
+    {
+      name: "Jeans",
+      price: "10.00 $",
+      img: "./images/jeans6.jfif",
+      index: "5",
+    },
   ],
   [
-    { name: "Shoes", price: "25.00 $", img: "./images/shoes.jfif" ,index:'0'},
-    { name: "Shoes", price: "17.00 $", img: "./images./shoes1.JPG" ,index:'1'},
-    { name: "Shoes", price: "10.00 $", img: "./images/shoes3.jfif" ,index:'2'},
-    { name: "Shoes", price: "17.00 $", img: "./images/shoes4.jfif" ,index:'3'},
-    { name: "Shoes", price: "25.00 $", img: "./images/shoes5.jfif" ,index:'4'},
-    { name: "Shoes", price: "10.00 $", img: "./images/shoes6.jfif" ,index:'5'},
+    { name: "Shoes", price: "25.00 $", img: "./images/shoes.jfif", index: "0" },
+    {
+      name: "Shoes",
+      price: "17.00 $",
+      img: "./images./shoes1.JPG",
+      index: "1",
+    },
+    {
+      name: "Shoes",
+      price: "10.00 $",
+      img: "./images/shoes3.jfif",
+      index: "2",
+    },
+    {
+      name: "Shoes",
+      price: "17.00 $",
+      img: "./images/shoes4.jfif",
+      index: "3",
+    },
+    {
+      name: "Shoes",
+      price: "25.00 $",
+      img: "./images/shoes5.jfif",
+      index: "4",
+    },
+    {
+      name: "Shoes",
+      price: "10.00 $",
+      img: "./images/shoes6.jfif",
+      index: "5",
+    },
   ],
 ];
 const items = $("#items");
@@ -32,9 +117,9 @@ const items3 = $("#items3");
 const slider_btn = $(".slider-btn");
 const home = $("#home");
 const itemShop = $("#addedItem");
-const allShopItem=$("#allItem")
-const homeTitle=$(".homeTitle")
-let counterShop=0
+const allShopItem = $("#allItem");
+const homeTitle = $(".homeTitle");
+let counterShop = 0;
 
 //-----------------------------------------------------------------
 
@@ -43,7 +128,7 @@ $("#home").on("click", function () {
   $("#items").hide();
   $("#items2").hide();
   $("#items3").hide();
-  
+
   $("#slider").show();
   $("#addedItem").hide();
   $("#allItem").show();
@@ -52,20 +137,20 @@ $("#home").on("click", function () {
 //-----------------------------------------------------------------
 //slider button
 
-const all= ()=> {
+const all = () => {
   $("#allItem").html("");
-for(j=0;j<allItems.length;j++){
-   for (i = 0; i < allItems[i].length; i++) {
-    const myItem = $(`<div class="item">
+  for (j = 0; j < allItems.length; j++) {
+    for (i = 0; i < allItems[j].length; i++) {
+      const myItem = $(`<div class="item">
       <img class="image" src=${allItems[j][i]["img"]}>
       <p>${allItems[j][i]["name"]}</p>
       <p class="price">${allItems[j][i]["price"]}</p>
       <button class=" btn3" onclick="getItem('${allItems[j][i].index}')">Add Item</button>
   </div>`);
-    myItem.appendTo(allShopItem);
+      myItem.appendTo(allShopItem);
+    }
   }
-}
- 
+
   $("#items").hide();
   $("#items2").hide();
   $("#items3").hide();
@@ -73,9 +158,8 @@ for(j=0;j<allItems.length;j++){
   $("#slider").show();
   $("#addedItem").hide();
   $("#allItem").show();
-  
 };
-$(".slider-btn").on("click",all )
+$(".slider-btn").on("click", all);
 //--------------------------------------------------------------------
 $(".slider-btnS").on("click", function () {
   $("#items").html("");
@@ -122,35 +206,33 @@ $("#t-shirt").on("click", function () {
 });
 //----------------------------------------------------------
 //array to added on cart
-const counter=document.querySelector(".counter")
-const array=[]
-const getItem=(elem)=>{
-  counterShop++
-counter.innerText=counterShop
-array.push(allItems[0][elem])
-console.log(elem)
-localStorage.setItem("cart22",JSON.stringify(array))
+const counter = document.querySelector(".counter");
+const array = [];
+const getItem = (elem) => {
+  counterShop++;
+  counter.innerText = counterShop;
+  array.push(allItems[0][elem]);
+  console.log(elem);
+  localStorage.setItem("cart22", JSON.stringify(array));
+};
+const getItemJeans = (elem) => {
+  counterShop++;
+  counter.innerText = counterShop;
+  array.push(allItems[1][elem]);
+  localStorage.setItem("cart22", JSON.stringify(array));
+};
 
-}
-const getItemJeans=(elem)=>{
-  counterShop++
-  counter.innerText=counterShop
-  array.push(allItems[1][elem])
-  localStorage.setItem("cart22",JSON.stringify(array))
-}
-
-const getShoesItem=(elem)=>{
-  counterShop++
-  counter.innerText=counterShop
-  array.push(allItems[2][elem])
-  localStorage.setItem("cart22",JSON.stringify(array))
-}
-
+const getShoesItem = (elem) => {
+  counterShop++;
+  counter.innerText = counterShop;
+  array.push(allItems[2][elem]);
+  localStorage.setItem("cart22", JSON.stringify(array));
+};
 
 //---------------------------------------------------------
 //cart button
 
- const getItemShop= ()=> {
+const getItemShop = () => {
   $("#items2").hide();
   $("#items3").hide();
   $(".homeTitle").hide();
@@ -159,12 +241,12 @@ const getShoesItem=(elem)=>{
   $("#addedItem").show();
   $("#allItem").hide();
 
-  itemShop.html(``)
+  itemShop.html(``);
 
   let fav = JSON.parse(localStorage.getItem("cart22")) || array;
- 
-  for(let i=0;i<fav.length;i++){
-  const myItem = $(`<div class="item">
+
+  for (let i = 0; i < fav.length; i++) {
+    const myItem = $(`<div class="item">
   <img class="image" src=${fav[i].img}>
   <p>${fav[i].name}</p>
   <p class="price">${fav[i].price}</p>
@@ -172,24 +254,22 @@ const getShoesItem=(elem)=>{
   <button class=" remove" onclick="removeItem('${i}')">remove</button>
 
 </div>`);
-myItem.appendTo(itemShop);
+    myItem.appendTo(itemShop);
   }
-  
 };
-$(".shop").on("click",getItemShop)
-const removeItem=(elem)=>{
-  if(counterShop>0){
+$(".shop").on("click", getItemShop);
+const removeItem = (elem) => {
+  if (counterShop > 0) {
     counterShop--;
-    counter.innerText=counterShop
+    counter.innerText = counterShop;
+  } else {
+    counter.innerText = counterShop;
   }
-    else
-  {counter.innerText=counterShop}
-  
-  array.splice(elem,1)
-  localStorage.setItem("cart22",JSON.stringify(array))
-  getItemShop()
- 
-} 
+
+  array.splice(elem, 1);
+  localStorage.setItem("cart22", JSON.stringify(array));
+  getItemShop();
+};
 //----------------------------------------------------------
 //jeans button
 $("#jeans").on("click", function () {
